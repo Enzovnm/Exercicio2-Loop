6c01ff570aeecdf87b28d600da491b779d848767
Enzo Vila Nova Monteiro <enzovila.monteiro@gmail.com>
2020-11-22 23:06:13 -0300

    Exercício 2 da Lista de loop em Java

---
+++ Loop2_EnzoVila/src/loop2_enzovila/Loop2_EnzoVila.java
@@ -0,0 +1,16 @@
+package loop2_enzovila;
+
+public class Loop2_EnzoVila {
+
+    public static void main(String[] args) {
+        int i=1000;
+        
+        do
+        {
+            System.out.println(i);
+            i += 2;        
+            
+        }
+        while(i<=1500);
+    }
+}
